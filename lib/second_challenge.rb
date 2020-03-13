db0756c0096054949a904f85c5ac5db787460e3f
Christopher Cid <iamcid17@gmail.com>
2020-03-13 17:18:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => { :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       
       }, 
-    :capulet => { :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => {}
+    :capulet => { :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
       }}
 
   
